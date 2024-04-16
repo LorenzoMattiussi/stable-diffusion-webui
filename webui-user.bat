5495a033c5752448d00a634747ea74ffec1f64bc
LorenzoMattiussi <mattiussi.lorenzo@gmail.com>
2024-04-16 15:38:37 +0200

    update

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\Lorenzo\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers
+set COMMANDLINE_ARGS=--xformers 
 set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
 call webui.bat
