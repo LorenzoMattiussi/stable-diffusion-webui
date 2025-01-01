835c44d7894a41ff682beb684fb3f7ad87e32948
LorenzoMattiussi <mattiussi.lorenzo@gmail.com>
2025-01-02 00:16:51 +0100

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --xformers --opt-split-attention
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
 
 call webui.bat
